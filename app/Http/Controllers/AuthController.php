0e9f47319794003f0a6c8e5a3ac0eedecddc4cae
techbabette <marinakrsticrf@gmail.com>
2024-02-13 14:00:42 +0100

    Basic JWT auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    /**
+     * Create a new AuthController instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function login()
+    {
+        $credentials = request(['email', 'password']);
+
+        if (! $token = auth()->attempt($credentials)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function register()
+    {
+        $credentials = request(['name', 'email', 'password']);
+
+        $query = new User();
+
+        $query::create($credentials);
+
+        return response()->json(['message' => 'Success'], 201);
+    }
+
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
+    public function logout()
+    {
+        auth()->logout();
+
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+
+    /**
+     * Refresh a token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function refresh()
+    {
+        return $this->respondWithToken(auth()->refresh());
+    }
+
+    protected function respondWithToken($token)
+    {
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AuthorController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\CategoryController;
@@ -26,3 +27,16 @@ Route::get("/category", [CategoryController::class, "index"]);
 Route::get("/author", [AuthorController::class, "index"]);
 
 Route::get("/book", [BookController::class, "index"]);
+
+ Route::group([
+
+     'middleware' => 'api',
+     'prefix' => 'auth'
+
+ ], function ($router) {
+     Route::post('login', [AuthController::class, "login"])->name('login');
+     Route::post('logout', [AuthController::class, "logout"]);
+     Route::post('refresh', [AuthController::class, "refresh"]);
+     Route::post('me', [AuthController::class, "me"]);
+     Route::post('register', [AuthController::class, "register"]);
+ });
