fdba692ce0b1c2cb8d4ded68bb15a99b1725f7d7
techbabette <marinakrsticrf@gmail.com>
2024-02-14 12:53:39 +0100

    Role seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AccessLevelSeeder::class,
             LinkSeeder::class,
+            RoleSeeder::class,
             CategorySeeder::class,
             AuthorSeeder::class
         ]);
