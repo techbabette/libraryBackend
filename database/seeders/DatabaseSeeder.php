b42baa232c22d44ce66a1f8e7e41aeaaf421b140
techbabette <marinakrsticrf@gmail.com>
2024-02-14 12:41:47 +0100

    Category seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             AccessLevelSeeder::class,
-            LinkSeeder::class
+            LinkSeeder::class,
+            CategorySeeder::class
         ]);
     }
 }
