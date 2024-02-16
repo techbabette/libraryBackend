658ee20d08879ff523d3380a1830df3cd657a1ab
techbabette <marinakrsticrf@gmail.com>
2024-02-16 07:48:37 +0100

    Email verification token model

--- database/migrations/76create_email_verification_tokens_table.php
+++ database/migrations/76create_email_verification_tokens_table.php
@@ -11,11 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
+        Schema::create('email_verification_tokens', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->foreignId('role_id')->constrained();
+            $table->string('token', 255);
+            $table->timestamps();
         });
     }
 
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('email_verification_tokens');
     }
 };
