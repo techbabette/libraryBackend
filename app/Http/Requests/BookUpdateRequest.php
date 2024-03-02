fcbb8c75140aaf30c6dec369d7fcafbb35acd360
techbabette <marinakrsticrf@gmail.com>
2024-03-02 07:25:19 +0100

    Prevent setting active book category/author to inactive category/author

--- app/Http/Requests/BookUpdateRequest.php
+++ app/Http/Requests/BookUpdateRequest.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Category;
+use Closure;
 use Illuminate\Foundation\Http\FormRequest;
 
 class BookUpdateRequest extends FormRequest
@@ -29,9 +33,46 @@ class BookUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            "id" => "required|exists:books,id",
-            'category_id' => 'required|exists:categories,id',
-            'author_id' => 'required|exists:authors,id',
+            "id" => [
+                "required",
+                "exists:books,id",
+                ],
+            'category_id' => [
+                "required", 
+                "exists:categories,id",
+                function(string $attribute, mixed $value, Closure $fail){
+                    $book = Book::withTrashed()->find($this->input('id'));
+                    $bookTrashed = $book->trashed();
+                    //If book is inactive, stop check and allow assignement to any category
+                    if($bookTrashed){
+                        return;
+                    }
+
+                    //If book is not inactive, check category
+                    $requestedCategory = Category::find($value);
+                    if(!$requestedCategory){
+                        $fail("Cannot set active book category to inactive category");
+                    }
+                }
+            ],
+            'author_id' => [
+                "required",
+                "exists:authors,id",
+                function(string $attribute, mixed $value, Closure $fail){
+                    $book = Book::withTrashed()->find($this->input('id'));
+                    $bookTrashed = $book->trashed();
+                    //If book is inactive, stop check and allow assignement to any author
+                    if($bookTrashed){
+                        return;
+                    }
+
+                    //If book is not inactive, check author
+                    $requestedAuthor = Author::find($value);
+                    if(!$requestedAuthor){
+                        $fail("Cannot set active book author to inactive author");
+                    }
+                }
+            ],
             'name' => 'required|max:30|string|min:3',
             'img' => 'mimes:jpg,png',
             'description' => 'required|string|min:30',
