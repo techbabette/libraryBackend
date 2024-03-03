de54646add4e0d601584170b71e51dd398f1ee1d
techbabette <marinakrsticrf@gmail.com>
2024-03-03 08:57:43 +0100

    Expand favorite seeder

--- database/seeders/FavoriteSeeder.php
+++ database/seeders/FavoriteSeeder.php
@@ -17,6 +17,14 @@ class FavoriteSeeder extends Seeder
             [
                 "book_id" => 1,
                 "user_id" => 1
+            ],
+            [
+                "book_id" => 2,
+                "user_id" => 2
+            ],
+            [
+                "book_id" => 3,
+                "user_id" => 2
             ]
         ];
 
