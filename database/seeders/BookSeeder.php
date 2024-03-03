6c3ee3854666faba09bd129defc1325a411be1cd
techbabette <marinakrsticrf@gmail.com>
2024-03-03 11:03:43 +0100

    Initial books have their own images

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -18,7 +18,7 @@ class BookSeeder extends Seeder
                 "category_id" => 1,
                 "author_id" => 2,
                 "name" => "The Art of War",
-                "img" => "image.png",
+                "img" => "the_art_of_war.jpg",
                 "description" => "The art of war is a good book",
                 "number_owned" => 2
             ],
@@ -26,7 +26,7 @@ class BookSeeder extends Seeder
                 "category_id" => 1,
                 "author_id" => 1,
                 "name" => "Sapiens",
-                "img" => "image.png",
+                "img" => "sapiens.jpg",
                 "description" => "A historical overview of human evolution and civilization, addressing how humans became the dominant species and shaped their societies, economies, and cultures",
                 "number_owned" => 2
             ],
@@ -34,7 +34,7 @@ class BookSeeder extends Seeder
                 "category_id" => 3,
                 "author_id" => 1,
                 "name" => "Homo Deus",
-                "img" => "image.png",
+                "img" => "homo_deus.jpg",
                 "description" => " Homo Deus explores the projects, dreams and nightmares that will shape the twenty-first century—from overcoming death to creating artificial life. It asks the fundamental questions: Where do we go from here? And how will we protect this fragile world from our own destructive powers? This is the next stage of evolution. This is Homo Deus.",
                 "number_owned" => 2
             ],
@@ -42,7 +42,7 @@ class BookSeeder extends Seeder
                 "category_id" => 5,
                 "author_id" => 8,
                 "name" => "The Art of Reading Minds",
-                "img" => "image.png",
+                "img" => "the_art_of_reading_minds.jpg",
                 "description" => "How would you like to know what the people around you are thinking? Do you want to network like a pro, persuade your boss to give you that promotion, and finally become the life of every party? Now, with Henrik Fexeus's expertise, you can. ",
                 "number_owned" => 2
             ],
@@ -50,17 +50,17 @@ class BookSeeder extends Seeder
                 "category_id" => 5,
                 "author_id" => 8,
                 "name" => "The Art of Social Excellence",
-                "img" => "image.png",
+                "img" => "the_art_of_social_excellence.jpg",
                 "description" => "Henrik teaches a type of social skill reserved for a privileged few – until now. People with social finesse are more often appointed leaders, gets promoted faster and can quickly create meaningful and deep relationships with others. ",
                 "number_owned" => 2
             ],
             [
-                "category_id" => 2,
-                "author_id" => 8,
-                "name" => "Some book",
-                "img" => "image.png",
-                "description" => "This is a placeholder book",
-                "number_owned" => 2
+                "category_id" => 3,
+                "author_id" => 5,
+                "name" => "Numbers don't lie",
+                "img" => "numbers_dont_lie.jpg",
+                "description" => "Vaclav Smil's mission is to make facts matter. An environmental scientist, policy analyst, and a hugely prolific author, he is Bill Gates' go-to guy for making sense of our world. In Numbers Don't Lie, Smil answers questions such as: What's worse for the environment--your car or your phone? How much do the world's cows weigh (and what does it matter)? And what makes people happy? ",
+                "number_owned" => 3
             ],
         ];
 
