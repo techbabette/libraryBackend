0f4b0882de277de62a33b0f8a19333eabea8f9ae
techbabette <marinakrsticrf@gmail.com>
2024-03-09 14:27:38 +0100

    Frontend now uses HTML5 history

--- resources/views/ConfirmationEmail.blade.php
+++ resources/views/ConfirmationEmail.blade.php
@@ -2,4 +2,4 @@
     Welcome to the City Library
 </h2>
 
-<p>Click <a href="{{ config('website.websiteUrl') }}/#/login/?user={{ $id }}&token={{ $token }}">this link</a> to activate your account</p>
+<p>Click <a href="{{ config('website.websiteUrl') }}/login/?user={{ $id }}&token={{ $token }}">this link</a> to activate your account</p>
